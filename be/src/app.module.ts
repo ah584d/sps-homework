3a63ae1e020208fc4971e63dfbe01738a56149e2
Avraham HAMU <avraham.hamu@beehero.io>
2023-10-11 10:54:03 +0300

    add empty files

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PropertiesModule } from './properties/properties.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PropertiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
