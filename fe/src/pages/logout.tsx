89316464e6050e18f07002c5027ceb5cc75d082c
Avraham HAMU <avraham.hamu@beehero.io>
2023-10-12 13:38:23 +0300

    logout page

--- fe/src/pages/logout.tsx
+++ fe/src/pages/logout.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../common/hooks/auth.hook';
+import styles from './Login.module.css';
 
 const Logout = () => {
     const { setToken } = useAuth();
@@ -14,7 +15,15 @@ const Logout = () => {
         handleLogout();
     }, 3 * 1000);
 
-    return <>Logout Page</>;
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.titleContainer}>
+                <div>Logout</div>
+            </div>
+
+            <br />
+        </div>
+    );
 };
 
 export default Logout;
