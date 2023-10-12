264024c2747bacf6b79cd55db6b312f31255eb4a
Avraham HAMU <avraham.hamu@beehero.io>
2023-10-12 12:56:03 +0300

    add login page

--- fe/src/pages/login.tsx
+++ fe/src/pages/login.tsx
@@ -1,10 +1,14 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../common/hooks/auth.hook";
+import { ReactElement, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../common/hooks/auth.hook';
 
-const Login = () => {
+const Login = (): ReactElement => {
     const { setToken } = useAuth();
     const navigate = useNavigate();
-
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
     const handleLogin = () => {
         setToken('this is a test token');
         navigate('/', { replace: true });
@@ -14,7 +18,49 @@ const Login = () => {
         handleLogin();
     }, 3 * 1000);
 
-    return <>Login Page</>;
+    const onButtonClick = () => {
+        // You'll update this function later...
+    };
+
+    return (
+        <div className={'mainContainer'}>
+            <div className={'titleContainer'}>
+                <div>Login</div>
+            </div>
+
+            <br />
+
+            <div className={'inputContainer'}>
+                <input
+                    value={email}
+                    placeholder="Enter your email here"
+                    onChange={(ev) => setEmail(ev.target.value)}
+                    className={'inputBox'}
+                />
+
+                <label className="errorLabel">{emailError}</label>
+            </div>
+
+            <br />
+
+            <div className={'inputContainer'}>
+                <input
+                    value={password}
+                    placeholder="Enter your password here"
+                    onChange={(ev) => setPassword(ev.target.value)}
+                    className={'inputBox'}
+                />
+
+                <label className="errorLabel">{passwordError}</label>
+            </div>
+
+            <br />
+
+            <div className={'inputContainer'}>
+                <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
+            </div>
+        </div>
+    );
 };
 
 export default Login;
