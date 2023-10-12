5881abd8b9104f686ecf2adcb3bffc71231b8715
Avraham HAMU <avraham.hamu@beehero.io>
2023-10-12 13:35:28 +0300

    fix login page

--- fe/src/pages/login.tsx
+++ fe/src/pages/login.tsx
@@ -1,34 +1,27 @@
 import { ReactElement, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../common/hooks/auth.hook';
+import styles from './Login.module.css';
 
 const Login = (): ReactElement => {
     const { setToken } = useAuth();
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
+    const [userNameError, setUserNameError] = useState('');
     const [passwordError, setPasswordError] = useState('');
+
     const handleLogin = () => {
         setToken('this is a test token');
         navigate('/', { replace: true });
     };
 
-    // setTimeout(() => {
-    //     handleLogin();
-    // }, 3 * 1000);
-
     const onButtonClick = () => {
-        setEmailError('');
+        setUserNameError('');
         setPasswordError('');
 
-        if ('' === email) {
-            setEmailError('Please enter your email');
-            return;
-        }
-
-        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-            setEmailError('Please enter a valid email');
+        if ('' === userName) {
+            setUserNameError('Please enter your username');
             return;
         }
 
@@ -41,44 +34,45 @@ const Login = (): ReactElement => {
             setPasswordError('The password must be 8 characters or longer');
             return;
         }
+        handleLogin();
     };
 
     return (
-        <div className={'mainContainer'}>
-            <div className={'titleContainer'}>
+        <div className={styles.mainContainer}>
+            <div className={styles.titleContainer}>
                 <div>Login</div>
             </div>
 
             <br />
 
-            <div className={'inputContainer'}>
+            <div className={styles.inputContainer}>
                 <input
-                    value={email}
-                    placeholder="Enter your email here"
-                    onChange={(ev) => setEmail(ev.target.value)}
-                    className={'inputBox'}
+                    value={userName}
+                    placeholder="Enter your username here"
+                    onChange={(ev) => setUserName(ev.target.value)}
+                    className={styles.inputBox}
                 />
 
-                <label className="errorLabel">{emailError}</label>
+                <label className={styles.errorLabel}>{userNameError}</label>
             </div>
 
             <br />
 
-            <div className={'inputContainer'}>
+            <div className={styles.inputContainer}>
                 <input
                     value={password}
                     placeholder="Enter your password here"
                     onChange={(ev) => setPassword(ev.target.value)}
-                    className={'inputBox'}
+                    className={styles.inputBox}
                 />
 
-                <label className="errorLabel">{passwordError}</label>
+                <label className={styles.errorLabel}>{passwordError}</label>
             </div>
 
             <br />
 
-            <div className={'inputContainer'}>
-                <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
+            <div className={styles.inputContainer}>
+                <input className={styles.inputButton} type="button" onClick={onButtonClick} value={'Log in'} />
             </div>
         </div>
     );
